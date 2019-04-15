f6a3fc5b04e89ba3cc3125230ed68a561b02f61d
mattfazza <matthewfazza@yahoo.com>
2019-04-15 14:34:26 -0600

    Working on the first lecture of Stanford's course (again)

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Mathews Fazza on 4/15/19.
+//  Copyright © 2019 Matt Fazza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //directive + func keyword + name of function(_ and sender are names of the parameter: UIButton is the type)
+    @IBAction func touchCard(_ sender: UIButton) {
+        print("bug");
+    }
+    
+}
+
