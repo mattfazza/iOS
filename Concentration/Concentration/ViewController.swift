8295dac5c89c58f3d353117b7c97a8d1cba09ded
mattfazza <matthewfazza@yahoo.com>
2019-04-15 18:18:04 -0600

    Updates to add more cards

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -10,23 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var flipCount = 0
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    var flipCount = 0 {
+        //property observer
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
     //var flipCount: Int = 0 is the same thing *could be initialized with a initializer as well, but iheritance makes it a bit tricky
-
+    
+    //array of card buttons
+    @IBOutlet var cardButtons: [UIButton]!
+    
     //directive + func keyword + name of function(_ and sender are names of the parameter: UIButton is the type) *_ means no arg since it's an internal ios who is sending it
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
-        flipCard(withEmoji: "🐛", on: sender) //notice that the argument name "on" matches the external argument name on the function definition
+        let cardNumber = cardButtons.index(of: sender)!
+        print("card number: \(cardNumber)")
+        
+        
+        
+        //flipCard(withEmoji: "🐛", on: sender) //notice that the argument name "on" matches the external argument name on the function definition
     }
     
-    @IBAction func touchSecondCard(_ sender: UIButton) {
-        flipCount += 1
-        flipCard(withEmoji: "🐜", on: sender)
-    }
     
     //internal name is emoji, withEmoji is the external one for the caller
     func flipCard(withEmoji emoji: String, on button: UIButton){
-        print("flipCard(withEmoji: \(emoji)")
         if button.currentTitle == emoji{
             button.setTitle("", for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 0.714150488, green: 0.6348293042, blue: 1, alpha: 1)
